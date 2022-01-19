6d3f716d1d33cf1d612bf44f0072e5509642b03c
Brandon Hunsaker <brandondwight42@gmail.com>
2022-01-19 09:45:42 -0700

    Initial commit

---
+++ findTheRange/findTheRange.js
@@ -0,0 +1,9 @@
+// Starting array
+let array = [28, 43, -12, 30, 4, 0, -36]
+
+let numbersLength = array.length;
+for ( let i = 0; i < numbersLength; i++) {
+    console.log (numbers[i]);
+}
+
+// Write your solution below:
